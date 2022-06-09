39fff845d28750bfef576378983b2b0f77f44dbd
ClementPan <clementpan2022@gmail.com>
2022-06-09 14:09:43 +0800

    feat: setup testing websocket

--- app.js
+++ app.js
@@ -5,22 +5,65 @@ const port = process.env.PORT || 3000
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
 const cors = require('cors')
-
-// require routes
-const routes = require('./routes')
-
-// require Todo
-const { urlencoded } = require('body-parser')
+const ws = require('ws');
 
 
-app.use(cors())
 
-app.use(methodOverride('_method'))
+////////////////////////
 
-app.use(bodyParser.urlencoded({ extended: true }))
+// Set up a headless websocket server that prints any
+// events that come in.
+const wsServer = new ws.Server({ noServer: true })
+wsServer.on('connection', socket => {
+  console.log('[[[ wsServer.on connection ');
 
-app.use(routes)
+  socket.on('message', message => {
+    console.log('[[[ socket.on message: ', message);
 
-app.listen(port, () => {
-  console.log(`The app is listening on http://localhost:${port}.`)
-})
+    wsServer.clients.forEach(function each(client) {
+      if (client.readyState === ws.OPEN) {
+        setTimeout(function () {
+          const message = {
+            "source": "server",
+            "content": "response from server",
+            "message": mesasge
+          }
+          const data = Buffer.from(JSON.stringify(message))
+          client.send(data, { binary: false });
+          console.log('[[[ client.send', message);
+        }, 1000);
+      }
+    });
+  });
+});
+
+const server = app.listen(port, () => {
+  console.log(`[[[ Websocket app listening at http://localhost:${port}`)
+});
+
+server.on('upgrade', (request, socket, head) => {
+  console.log('[[[ server.on upgrade');
+  wsServer.handleUpgrade(request, socket, head, socket => {
+    wsServer.emit('connection', socket, request);
+  });
+});
+////////////////////////
+
+// // require routes
+// const routes = require('./routes')
+
+// // require Todo
+// const { urlencoded } = require('body-parser')
+
+
+// app.use(cors())
+
+// app.use(methodOverride('_method'))
+
+// app.use(bodyParser.urlencoded({ extended: true }))
+
+// app.use(routes)
+
+// app.listen(port, () => {
+//   console.log(`The app is listening on http://localhost:${port}.`)
+// })
